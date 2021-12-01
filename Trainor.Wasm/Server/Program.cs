0c2a74cda294bd66b319a472bf4adbdd05df125a
oskarbreindahl <83770676+oskarbreindahl@users.noreply.github.com>
2021-12-01 14:45:01 +0100

    New branch "develop"

--- Trainor.Wasm/Server/Program.cs
+++ Trainor.Wasm/Server/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Trainor.Wasm.Server
 {
