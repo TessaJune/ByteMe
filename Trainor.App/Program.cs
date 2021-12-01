c42b643c2086fd26705ce2f91045afe63490039b
simonlk <simon@kristianzen.dk>
2021-12-01 15:21:57 +0100

    remove binary build for storage (no entrypoint)

---
+++ Trainor.App/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Trainor.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
