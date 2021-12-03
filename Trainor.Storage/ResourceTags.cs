2e1370d9759f4d9f8cf772700b9963b8f62dcba0
simonlk <simon@kristianzen.dk>
2021-12-03 14:22:02 +0100

    add untracked file

---
+++ Trainor.Storage/ResourceTags.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainor.Storage
+{
+    public enum TypeTag
+    {
+        VIDEO,
+        DOCUMENT,
+        PICTURE,
+    }
+    public enum SubjectTag
+    {
+        C_SHARP,
+        JAVA,
+    }
+}
