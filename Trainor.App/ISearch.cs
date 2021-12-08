47d53d7c1ab32f85d7c353921186ac83cda1abed
freyjabun <freyjabun@gmail.com>
2021-12-08 13:11:37 +0100

    Set up v basic search interface and added overleaf document

--- Trainor.App/ISearch.cs
+++ Trainor.App/ISearch.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Trainor.Storage.Entities;
 
 namespace Trainor.App
 {
     public interface ISearch
     {
-
+        IEnumerable<ResourceDto> SearchByKeyword(string keyword);
+        IEnumerable<ResourceDto> SearchByFilter(string filter);
     }
 }
--- Trainor.App/Search.cs
+++ Trainor.App/Search.cs
@@ -2,17 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Trainor.Storage;
 using Trainor.Storage.Entities;
 
 namespace Trainor.App
 {
     public class Search : ISearch
     {
+        public IEnumerable<ResourceDto> SearchByFilter(string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ResourceDto> SearchByKeyword(string keyword)
+        {
+            throw new NotImplementedException();
+            //var resource = new DataContext(resource);
+        }
 
         private IEnumerable<ResourceDetailsDto> GetBySearchQuery(Func<ResourceDetailsDto, ResourceDetailsDto, bool> property)
         {
             throw new NotImplementedException();
         }
-
     }
 }
