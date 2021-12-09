24764e13d60b512a4480aa3bc3cdcacdaf2885db
freyjabun <freyjabun@gmail.com>
2021-12-09 15:38:43 +0100

    Added ResourceComparator to check equality between ResourceDTOs and ResourceDetailsDTOs

--- Trainor.Storage/ResourceComparator.cs
+++ Trainor.Storage/ResourceComparator.cs
@@ -1,10 +1,33 @@
+using System;
 using Trainor.Storage.Entities;
 
-namespace Trainer.Storage;
-public class ResourceComparator<ResourceDto>
-{
-    public void Compare()
+namespace Trainer.Storage {
+    public static class ResourceComparator
     {
+        public static bool IsEqual(this ResourceDto r1, ResourceDto r2)
+        {
+            if(r1.name != r2.name) return false;
+            if(r1.authors != r2.authors) return false;
+            return true;
+        }
 
+        public static bool IsEqual(this ResourceDto r1, ResourceDetailsDto r2)
+        {
+            if(r1.name != r2.name) return false;
+            if(r1.authors != r2.authors) return false;
+            return true;
+        }
+        public static bool IsEqual(this ResourceDetailsDto r1, ResourceDto r2)
+        {
+            if(r1.name != r2.name) return false;
+            if(r1.authors != r2.authors) return false;
+            return true;
+        }
+        public static bool IsEqual(this ResourceDetailsDto r1, ResourceDetailsDto r2)
+        {
+            if(r1.name != r2.name) return false;
+            if(r1.authors != r2.authors) return false;
+            return true;
+        }
     }
 }
